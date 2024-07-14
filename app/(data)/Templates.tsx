824dbcd0401561bc94245a114c9da53f57498c1a
ShuhaiYu <1141071640@qq.com>
2024-07-14 13:59:22 +1000

    feat: Add Templates.tsx file for managing  templates

---
+++ app/(data)/Templates.tsx
@@ -0,0 +1,23 @@
+export default[
+    {
+        name:"Blog Title",
+        desc:'An AI tool that generates blog titles based on the content of the blog',
+        category:'Blog',
+        icon:"https://cdn-icons-png.flaticon.com/128/3959/3959542.png",
+        aiPrompt:"Generate a blog title based on the content of the blog",
+        slug:"generate-blog-title",
+        form:[
+            {
+                label:"Enter the content of the blog",
+                field:"input",
+                name:"content",
+                required:true,
+            },
+            {
+                label:"Enter the keyword",
+                field:"input",
+                name:"keyword",
+            }
+        ]
+    }
+]
