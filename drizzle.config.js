c8b8e88b70d519b75b009400ea3ef019932e483d
ShuhaiYu <1141071640@qq.com>
2024-07-15 23:08:11 +1000

    feat: Add drizzle configuration file

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+import { NEXT_BODY_SUFFIX } from "next/dist/lib/constants";
+
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.tsx",
+  dialect: "postgresql", // "mysql" | "sqlite" | "postgresql"
+  out: "./drizzle",
+  dbCredentials: {
+    url: 'postgresql://neondb_owner:Zokr4C6cngDU@ep-dry-base-a78fie2u.ap-southeast-2.aws.neon.tech/ai-content-generator?sslmode=require'
+  },
+};
